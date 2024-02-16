5865a942b16e0e22e95f7b47098c04e067d8d994
jalalm07 <jalalmujawar25@gmail.com>
2024-02-16 11:07:06 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
         for(String op : operations){
-            if(op.equals("++X") || op.equals("X++"))
+            if(op.chatAt(1) == '+')
                 x = x + 1;
             else x = x - 1;   
         }
