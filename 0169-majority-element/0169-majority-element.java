776c90f41cd6b9cd172016f9a330d95aa2d29d7e
jalalm07 <jalalmujawar25@gmail.com>
2024-06-05 13:02:08 -0500

    Time: 1 ms (99.8%), Space: 53.1 MB (31.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        if(nums.length == 1) return nums[0];
-
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int mark = nums.length / 2;
-        for(int num : nums){
-            if(countMap.containsKey(num)){
-                if(countMap.get(num) + 1 > mark) return num;
-                else countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-            } else countMap.put(num, 1);
+        // using moore's voting algo
+        int major=nums[0], count = 1;
+        for(int i=1; i<nums.length;i++){
+            if(count==0){
+                count++;
+                major=nums[i];
+            }else if(major==nums[i]){
+                count++;
+            }else count--;
+            
         }
-        return -1;
+        return major;
+
+        // if(nums.length == 1) return nums[0];
+
+        // Map<Integer, Integer> countMap = new HashMap<>();
+        // int mark = nums.length / 2;
+        // for(int num : nums){
+        //     if(countMap.containsKey(num)){
+        //         if(countMap.get(num) + 1 > mark) return num;
+        //         else countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        //     } else countMap.put(num, 1);
+        // }
+        // return -1;
+
+
     }
 }
