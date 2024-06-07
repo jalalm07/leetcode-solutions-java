32d3c1ae2f5b53febbd11d7366490f592846ee01
jalalm07 <jalalmujawar25@gmail.com>
2024-06-06 22:53:32 -0500

    Time: 0 ms (100%), Space: 42.7 MB (79.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -13,20 +13,18 @@ class Solution {
             }
         }
 
-        System.out.println(index);
-
-        if(index == -1)
+        if(index == -1){
             reverse(nums, 0, n-1);
-        else{
+            return;
+        }
+            
         for(int i = n -1; i > index; i--){
             if(nums[i] > nums[index]){
                 swap(nums, i, index);
                 break;
             }
         }
-
         reverse(nums, index+1, n-1);
-        }
 
     }
 
