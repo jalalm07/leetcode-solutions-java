af8c3ba79f646e78b2dd80cfee60fb2b5aa7fa59
jalalm07 <jalalmujawar25@gmail.com>
2024-06-26 18:59:28 -0500

    Time: 0 ms (100%), Space: 41.8 MB (70.54%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,16 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot =  findPivot(nums);
-        if(pivot == -1){
-            return binarySearch(nums, 0, nums.length - 1, target);
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // if mid points to the target
+            if (nums[mid] == target)
+                return mid;
+
+            // if left part is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    // element exists
+                    high = mid - 1;
+                } else {
+                    // element does not exist
+                    low = mid + 1;
+                }
+            } else { // if right part is sorted
+                if (nums[mid] <= target && target <= nums[high]) {
+                    // element exists
+                    low = mid + 1;
+                } else {
+                    // element does not exist
+                    high = mid - 1;
+                }
+            }
         }
-        if(target == nums[pivot]){
-            return pivot;
-        }
-        if(target >= nums[0]){
-            return binarySearch(nums, 0, pivot - 1, target);
-        }
-        return binarySearch(nums, pivot + 1, nums.length -1, target);
+        return -1;
     }
 
     int findPivot(int[] nums){
