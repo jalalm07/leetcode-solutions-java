d633759c30c336611cd56cf97736d85012b8e11d
jalalm07 <jalalmujawar25@gmail.com>
2024-06-26 19:00:39 -0500

    Time: 0 ms (100%), Space: 42.1 MB (28.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -29,38 +29,4 @@ class Solution {
         }
         return -1;
     }
-
-    int findPivot(int[] nums){
-        int start = 0;
-        int end = nums.length - 1;
-        while(start <= end){
-            int mid = start + (end -start) / 2;
-            if(mid < end && nums[mid] > nums[mid + 1]){
-                return mid;
-            } 
-            if (mid > start && nums[mid] < nums[mid - 1]){
-                return mid - 1;
-            } 
-            if (nums[mid] <= nums[start]){
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return -1;
-    }
-
-    int binarySearch(int[] nums, int start, int end, int target){
-        while (start <= end){
-            int mid = (start + end)/2;
-            if(nums[mid] == target){
-                return mid;
-            } else if (target > nums[mid]){
-                 start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
 }
