5dff2783ca9e9c19a1a1a0b60cb921070b2227dd
jalalm07 <jalalmujawar25@gmail.com>
2024-07-13 12:11:38 -0500

    Time: 0 ms (100%), Space: 42.2 MB (5.22%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,28 +15,41 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-            return false;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
-        while(!queue.isEmpty()){
-            TreeNode left = queue.remove();
-            TreeNode right = queue.remove();
-            if(left == null && right == null){
-                continue;
-            }   
-            if(left == null || right == null){
-                return false;
-            }    
-            if(left.val != right.val){
-                return false;        
-            }    
-            queue.add(left.left);
-            queue.add(right.right);
-            queue.add(left.right);
-            queue.add(right.left);
-        }
-        return true;    
+        
+        //solution1
+        if(root == null) return true;
+        return isSymmetricHelper(root.left, root.right);    
+
+        //solution 2
+        // if(root == null)
+        //     return false;
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root.left);
+        // queue.add(root.right);
+        // while(!queue.isEmpty()){
+        //     TreeNode left = queue.remove();
+        //     TreeNode right = queue.remove();
+        //     if(left == null && right == null){
+        //         continue;
+        //     }   
+        //     if(left == null || right == null){
+        //         return false;
+        //     }    
+        //     if(left.val != right.val){
+        //         return false;        
+        //     }    
+        //     queue.add(left.left);
+        //     queue.add(right.right);
+        //     queue.add(left.right);
+        //     queue.add(right.left);
+        // }
+        // return true;    
+    }
+
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right){
+        if(left == null || right == null)
+            return left == right;
+        if(left.val != right.val) return false;
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
 }
