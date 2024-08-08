edf6662a27edf9f9a884eacf5fa45672a3c64959
jalalm07 <jalalmujawar25@gmail.com>
2024-08-07 22:34:42 -0500

    Time: 0 ms (100%), Space: 41.4 MB (89.73%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -50,6 +50,7 @@ class Solution {
         if(left == null || right == null)
             return left == right;
         if(left.val != right.val) return false;
-        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+        return isSymmetricHelper(left.left, right.right) && 
+                    isSymmetricHelper(left.right, right.left);
     }
 }
