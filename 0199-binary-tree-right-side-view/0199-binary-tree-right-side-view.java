9e610be4c7f0b2090886043217044140d8c57779
jalalm07 <jalalmujawar25@gmail.com>
2024-08-08 18:04:44 -0500

    Time: 0 ms (100%), Space: 42 MB (40.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,6 +15,17 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+       
+       //solution
+       List<Integer> result = new ArrayList<Integer>();
+       rightView(root, result, 0);
+       
+       
+       /*
+       
+       Solution using level order traversal
+
+
         List<Integer> result = new ArrayList<>();
         if(root == null)
             return result;
@@ -32,6 +43,14 @@ class Solution {
                     result.add(curr.val);
             }
         } 
-        return result;  
+        return result;  */
+        return result;
+    }
+
+    public void rightView(TreeNode node, List<Integer> result, int level){
+        if(node == null) return;
+        if(result.size() == level) result.add(node.val);
+        rightView(node.right, result, level+1);
+        rightView(node.left, result, level+1);
     }
 }
