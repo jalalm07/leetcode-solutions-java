40c27a548437531e1a31f80f79076af113c1a641
jalalm07 <jalalmujawar25@gmail.com>
2024-08-09 22:24:55 -0500

    Time: 0 ms (100%), Space: 45.2 MB (47.8%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -1,32 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         TreeNode newNode = new TreeNode(val);
-        if(root == null) return newNode;
+        if (root == null)
+            return newNode;
         TreeNode curr = root;
-        while(true){
-            if(curr.val > val){
-                if(curr.left != null) curr = curr.left;
+        while (true) {
+            if (curr.val > val) {
+                if (curr.left != null)
+                    curr = curr.left;
                 else {
                     curr.left = newNode;
                     break;
                 }
             } else {
-                if(curr.right != null) curr = curr.right;
+                if (curr.right != null)
+                    curr = curr.right;
                 else {
                     curr.right = newNode;
                     break;
