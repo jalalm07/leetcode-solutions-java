62f1f8ffc03407dc9577d664a98af5d55f310e2e
jalalm07 <jalalmujawar25@gmail.com>
2024-08-23 09:47:02 -0500

    Time: 1 ms (99.89%), Space: 48.4 MB (67.11%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,29 +1,43 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Map<ListNode, Integer> map = new HashMap<>();
-        ListNode temp = headA;
-        while(temp != null){
-            map.put(temp, 1);
-            temp = temp.next;
+
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+
+        while (t1 != t2) {
+            t1 = t1 == null ? headB : t1.next;
+            t2 = t2 == null ? headA : t2.next;
         }
 
-        temp = headB;
+        return t1;
 
-        while(temp != null){
-            if(map.containsKey(temp)) return temp;
-            temp = temp.next;
-        }
-        return null;
+        // solution 2 using map
+        /*
+         * Map<ListNode, Integer> map = new HashMap<>();
+         * ListNode temp = headA;
+         * while(temp != null){
+         * map.put(temp, 1);
+         * temp = temp.next;
+         * }
+         * 
+         * temp = headB;
+         * 
+         * while(temp != null){
+         * if(map.containsKey(temp)) return temp;
+         * temp = temp.next;
+         * }
+         * return null;
+         */
     }
 }
