ca0f596ef24a78d19357c8168e47e44ed19126d0
jalalm07 <jalalmujawar25@gmail.com>
2024-08-27 10:38:21 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> chStack = new Stack<>();
+        char[] chArr = s.toCharArray();
+        for(char ch : chArr){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                chStack.push(ch);
+            }
+            else if(ch == ')'){
+                if(chStack.pop() != '(') return false;
+            }
+            else if(ch == ']'){
+                if(chStack.pop() != '[') return false;
+            }
+            else if(ch == '}'){
+                if(chStack.pop() != '{') return false;
+            }
+        }
+        return chStack.isEmpty();
+    }
+}
