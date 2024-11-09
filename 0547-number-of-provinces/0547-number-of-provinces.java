30ef604dac0b5356408684ed5dccc9cce2feefa8
jalalm07 <jalalmujawar25@gmail.com>
2024-11-09 12:00:37 -0600

    Time: 1 ms (91.12%), Space: 47.8 MB (36.02%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -18,7 +18,7 @@ class Solution {
 
     private void dfs(int i) {
         vis[i] = true;
-        for (int j = 0; j < adj.length; ++j) {
+        for (int j = 0; j < adj.length; j++) {
             if (!vis[j] && adj[i][j] == 1) {
                 dfs(j);
             }
