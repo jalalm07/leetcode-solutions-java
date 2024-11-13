7d3aacc240424e1b385e083468e76ebaeae8f329
jalalm07 <jalalmujawar25@gmail.com>
2024-11-13 10:59:46 -0600

    Time: 0 ms (100%), Space: 45 MB (35.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,13 +6,13 @@ class Solution {
         return ans;
     }
 
-    public static void fill(int[][] image, int sr, int sc, int color, int newColor){
-        if(sr >= 0 && sr < image.length &&  sc >= 0 && sc < image[0].length && image[sr][sc] == color){
+    public static void fill(int[][] image, int sr, int sc, int startPixelColor, int newColor){
+        if(sr >= 0 && sr < image.length &&  sc >= 0 && sc < image[0].length && image[sr][sc] == startPixelColor){
             image[sr][sc] = newColor;
-            fill(image, sr, sc - 1, color, newColor);
-            fill(image, sr - 1, sc, color, newColor);
-            fill(image, sr, sc + 1, color, newColor);
-            fill(image, sr + 1, sc, color, newColor);
+            fill(image, sr, sc - 1, startPixelColor, newColor);
+            fill(image, sr - 1, sc, startPixelColor, newColor);
+            fill(image, sr, sc + 1, startPixelColor, newColor);
+            fill(image, sr + 1, sc, startPixelColor, newColor);
         }
     }
 }
