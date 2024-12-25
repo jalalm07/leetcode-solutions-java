86765d609d50b4d7b4f2afcc96dc8a1bac0553b9
jalalm07 <jalalmujawar25@gmail.com>
2024-12-25 15:43:51 -0600

    Time: 4 ms (41.92%), Space: 42.7 MB (35.04%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -28,33 +28,31 @@ class Solution {
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Tuple> queue = new LinkedList<Tuple>();
-        queue.offer(new Tuple(root, 0, 0));
+        Queue<Tuple> queue = new LinkedList<>();
+        queue.add(new Tuple(root, 0, 0));
         while(!queue.isEmpty()){
-            Tuple tuple = queue.poll();
-            TreeNode node = tuple.node;
-            int row = tuple.row;
-            int col = tuple.col;
+            TreeNode node = queue.peek().node;
+            int row = queue.peek().row;
+            int col = queue.peek().col;
+            queue.remove();
 
             if(!map.containsKey(row)) map.put(row, new TreeMap<>());
             if(!map.get(row).containsKey(col)) map.get(row).put(col, new PriorityQueue<>());
 
-            map.get(row).get(col).offer(node.val);
+            map.get(row).get(col).add(node.val);
 
-            if(node.left != null) queue.offer(new Tuple(node.left, row - 1, col + 1));
-            if(node.right != null) queue.offer(new Tuple(node.right, row + 1, col + 1));
-
+            if(node.left != null) queue.add(new Tuple(node.left, row -1, col + 1));
+            if(node.right != null) queue.add(new Tuple(node.right, row + 1, col + 1));
         }
 
-        List<List<Integer>> answer = new ArrayList<>();
-        for(TreeMap<Integer, PriorityQueue<Integer>> el  : map.values()){
-            answer.add(new ArrayList<Integer>());
-            for(PriorityQueue<Integer> nodes: el.values()){
-                while(!nodes.isEmpty()){
-                    answer.get(answer.size() - 1).add(nodes.poll());
-                }
-            }
+        List<List<Integer>> result = new ArrayList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> element : map.values()){
+            result.add(new ArrayList<>());
+            for(PriorityQueue<Integer> pq : element.values()){
+                while(!pq.isEmpty())
+                    result.get(result.size() - 1).add(pq.poll());
+            } 
         }
-        return answer;
+        return result;
     }
 }
