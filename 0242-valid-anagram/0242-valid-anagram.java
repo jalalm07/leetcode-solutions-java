0ac946fc159960c98f8e008849fea9ee75642009
jalalm07 <jalalmujawar25@gmail.com>
2024-12-29 13:02:47 -0600

    Time: 5 ms (54.65%), Space: 42.9 MB (86.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        /*if(s.length() != t.length()) return false;
+        /*if(s.length() != t.length()) return false; // 3ms
         char[] s1 = s.toCharArray();
         char[] t1 = t.toCharArray();
         Arrays.sort(s1);
         Arrays.sort(t1);
         return Arrays.equals(s1, t1); */
 
+        //hashmap solution is slower than above sorting solution. //27ms
+        /*
         if(s.length() != t.length()) return false;
         Map<Character, Integer> smap = new HashMap<>();
         Map<Character, Integer> tmap = new HashMap<>();
@@ -17,5 +19,18 @@ class Solution {
             tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i),0) + 1);
         }
         return smap.equals(tmap);
+        */
+
+        if(s.length() != t.length()) return false;
+        int[] charCount = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int n: charCount){
+            if(n != 0) return false;
+        }
+        return true;
     }
 }
