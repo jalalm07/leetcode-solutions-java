ac61242a86daba7d9729386dee1408deed1caed1
jalalm07 <jalalmujawar25@gmail.com>
2024-12-29 12:50:10 -0600

    Time: 27 ms (6.2%), Space: 45 MB (9.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+
+        /*if(s.length() != t.length()) return false;
         char[] s1 = s.toCharArray();
         char[] t1 = t.toCharArray();
         Arrays.sort(s1);
         Arrays.sort(t1);
-        return Arrays.equals(s1, t1);
+        return Arrays.equals(s1, t1); */
+
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i),0) + 1);
+            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i),0) + 1);
+        }
+        return smap.equals(tmap);
     }
 }
