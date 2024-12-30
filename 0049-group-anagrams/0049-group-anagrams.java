8174937a821466bc747df8753babb3a656e59dd9
jalalm07 <jalalmujawar25@gmail.com>
2024-12-30 13:13:50 -0600

    Time: 7 ms (63.82%), Space: 47.8 MB (57.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> answer = new ArrayList<>();
-        if(strs.length == 1) {
-            List<String> ls = new ArrayList<String>();
-            ls.add(strs[0]);
-            answer.add(ls);
-            return answer;
-        }
+         List<List<String>> answer = new ArrayList<>();
+        // if(strs.length == 1) {
+        //     List<String> ls = new ArrayList<String>();
+        //     ls.add(strs[0]);
+        //     answer.add(ls);
+        //     return answer;
+        // }
         Map<String, List<String>> map = new HashMap<>();
         for(String s: strs){
             char[] ch = s.toCharArray();
             Arrays.sort(ch);
             String sorted = new String(ch);
-            if(map.containsKey(sorted)){
-                List<String> lnew = map.get(sorted);
-                lnew.add(s);
-                map.put(sorted, lnew);
-            } else{
-                List<String> lnew = new ArrayList<>();
-                lnew.add(s);
-                map.put(sorted, lnew);
-            }
+            List<String> lnew = map.getOrDefault(sorted, new ArrayList<String>());
+            lnew.add(s);
+            map.put(sorted, lnew);
         }
         for(List<String> ls : map.values()){
             answer.add(ls);
