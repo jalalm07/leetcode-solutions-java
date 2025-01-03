ea8044e6690548a6e04bf0ca56f15bb2c493f8bf
jalalm07 <jalalmujawar25@gmail.com>
2025-01-02 20:00:24 -0600

    Time: 28 ms (78.31%), Space: 51.5 MB (54.43%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int lastI, lastJ, lastK = 0;
-        int k = nums.length - 1;
-        List ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
-            lastI = nums[i];
+        int n  = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
             int j = i + 1;
+            int k = n - 1;
             while (j < k) {
-                lastJ = nums[j];
-                lastK = nums[k];
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    ans.add(list);
-                    while (j < k && nums[j] == lastJ) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == lastK) {
-                        k--;
-                    }
-                } else if (sum < 0) {
+                if (sum < 0) {
                     j++;
-                } else
+                } else if (sum > 0) {
                     k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
             }
         }
         return ans;
     }
+
 }
