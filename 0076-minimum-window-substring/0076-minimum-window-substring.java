2caf4721bd5509ea3fd52738c237bea1ed9a2430
jalalm07 <jalalmujawar25@gmail.com>
2025-01-05 20:49:16 -0600

    Time: 321 ms (5.12%), Space: 45.6 MB (19.21%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.isEmpty()) return "";
-
+        if(s.length() < t.length()) return "";
         Map<Character, Integer> countT = new HashMap<>();
-        Map<Character, Integer> window = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        Map<Character, Integer> countS = new HashMap<>();
+        
+        for(int i = 0; i < t.length(); i++){
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        int have = 0, need = countT.size();
-        int[] res = {-1, -1};
-        int resLen = Integer.MAX_VALUE;
         int l = 0;
+        int r = t.length() -1;
+        int[] ansIndex = new int[2];
+        String ans = "";
+        int minWindow = Integer.MAX_VALUE;
 
-        for (int r = 0; r < s.length(); r++) {
-            char c = s.charAt(r);
-            window.put(c, window.getOrDefault(c, 0) + 1);
-
-            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
-                have++;
-            }
-
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
-                    res[0] = l;
-                    res[1] = r;
+        while(r < s.length() && l <= s.length() - t.length()){
+            if(matches(countT, countS)){
+                if(r - l + 1 < minWindow){
+                    minWindow = r - l + 1;
+                    //ans = s.substring(l, r + 1);
+                    ansIndex[0] = l;
+                    ansIndex[1] = r + 1;
                 }
-
-                char leftChar = s.charAt(l);
-                window.put(leftChar, window.get(leftChar) - 1);
-                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
-                    have--;
-                }
+                countS.put(s.charAt(l), countS.get(s.charAt(l)) - 1);
                 l++;
+            } else {
+                r++;
+                if(r < s.length())
+                countS.put(s.charAt(r), countS.getOrDefault(s.charAt(r),0) + 1);
             }
         }
+        return s.substring(ansIndex[0], ansIndex[1]);
+    }
 
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+    private static boolean matches(Map<Character, Integer> countT, Map<Character, Integer> countS){
+        for(Character c : countT.keySet()){
+            if(countS.containsKey(c)){
+                if(countS.get(c) < countT.get(c)){
+                    return false;
+                }
+            } else return false;
+        }
+         return true;
     }
 }
