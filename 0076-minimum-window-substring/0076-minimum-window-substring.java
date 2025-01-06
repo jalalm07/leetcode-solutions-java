c330f4ae3cca4d125d6e379666cd2bad77e32cba
jalalm07 <jalalmujawar25@gmail.com>
2025-01-05 20:47:17 -0600

    Time: 19 ms (52.9%), Space: 45.4 MB (31.63%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,54 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
+        if (t.isEmpty()) return "";
 
-        // Frequency map for characters in `t`
         Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
         for (char c : t.toCharArray()) {
             countT.put(c, countT.getOrDefault(c, 0) + 1);
         }
 
-        // Two pointers for sliding window
-        int l = 0, r = 0;
-        int required = countT.size(); // Number of unique characters in `t` to be matched
-        int formed = 0; // Number of unique characters that are fully matched in current window
-        Map<Character, Integer> countS = new HashMap<>();
+        int have = 0, need = countT.size();
+        int[] res = {-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        int l = 0;
 
-        // Variables to track the minimum window
-        int[] ans = {-1, 0, 0}; // {window length, left, right}
-
-        while (r < s.length()) {
-            // Add the current character to the window
+        for (int r = 0; r < s.length(); r++) {
             char c = s.charAt(r);
-            countS.put(c, countS.getOrDefault(c, 0) + 1);
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            // Check if this character matches the frequency in `t`
-            if (countT.containsKey(c) && countS.get(c).intValue() == countT.get(c).intValue()) {
-                formed++;
+            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+                have++;
             }
 
-            // Try to shrink the window from the left
-            while (l <= r && formed == required) {
-                // Update the minimum window
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+            while (have == need) {
+                if ((r - l + 1) < resLen) {
+                    resLen = r - l + 1;
+                    res[0] = l;
+                    res[1] = r;
                 }
 
-                // Remove the leftmost character
                 char leftChar = s.charAt(l);
-                countS.put(leftChar, countS.get(leftChar) - 1);
-                if (countT.containsKey(leftChar) && countS.get(leftChar).intValue() < countT.get(leftChar).intValue()) {
-                    formed--;
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                    have--;
                 }
                 l++;
             }
-
-            // Expand the window
-            r++;
         }
 
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
     }
 }
