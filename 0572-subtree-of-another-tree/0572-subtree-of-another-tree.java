a25a6025193443bb594aa8652d76f3278a48b2d3
jalalm07 <jalalmujawar25@gmail.com>
2025-01-21 20:26:32 -0600

    Time: 3 ms (21.46%), Space: 44.3 MB (80.3%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,16 +15,24 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null) return false;
-        if(isSameTree(root, subRoot)) return true;
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode node = null;
+        while(!queue.isEmpty()){
+            node = queue.remove();
+            if(node.val == subRoot.val) {
+                if(isSame(node, subRoot)) return true;
+            }
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+        }
+        return false;//isSame(node, subRoot);        
     }
 
-    private static boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == q) return true;
-        if(p != null && q != null && p.val == q.val){
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }
-        return false;
+    private boolean isSame(TreeNode root, TreeNode subRoot){
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+        if(root.val != subRoot.val) return false;
+        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
     }
 }
