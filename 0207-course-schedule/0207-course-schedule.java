b6df28404a4f2e7eab0af9d5458d48b0754c47dc
jalalm07 <jalalmujawar25@gmail.com>
2025-02-05 10:16:54 -0600

    Time: 7 ms (50.5%), Space: 45.8 MB (36.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,67 @@
 class Solution {
+    
+    private Map<Integer, List<Integer>> preMap = new HashMap<>();
+    private Set<Integer> visited = new HashSet<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int m = prerequisites.length;
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         for(int i = 0; i < numCourses; i++){
+            preMap.put(i, new ArrayList<>());
+        }
+
+        for(int[] pre: prerequisites){
+            preMap.get(pre[0]).add(pre[1]);
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            if(!dfs(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int crs){
+        if(visited.contains(crs)) return false;
+        if(preMap.get(crs).isEmpty()) return true;
+        visited.add(crs);
+        for(int pre: preMap.get(crs)){
+            if(!dfs(pre)){
+                return false;
+            }
+        }
+        visited.remove(crs);
+        preMap.put(crs, new ArrayList<>());
+        return true;
+    }
+    /*
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
             list.add(new ArrayList<>());
         }
 
         int[] inDegree = new int[numCourses];
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < prerequisites.length; i++) {
             list.get(prerequisites[i][1]).add(prerequisites[i][0]);
             inDegree[prerequisites[i][0]]++;
         }
 
-        // for(int i = 0; i < numCourses; i++){
-        //     for(int n: list.get(i)){
-        //         inDegree[prerequisites[i][0]]++;
-        //     }
-        // }
-
         Queue<Integer> queue = new LinkedList<>();
 
-        for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0) queue.add(i);
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0)
+                queue.add(i);
         }
         int cnt = 0;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int node = queue.remove();
             cnt++;
-            for(int adjN: list.get(node)){
-                //inDegree[adjN]--;
-                if(--inDegree[adjN] == 0) queue.add(adjN);
+            for (int adjN : list.get(node)) {
+                if (--inDegree[adjN] == 0)
+                    queue.add(adjN);
             }
         }
 
         return cnt == numCourses;
     }
+    */
 }
