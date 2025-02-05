fe17450bfc7d008ae39d69e79e0bb1e2f7b79fd5
jalalm07 <jalalmujawar25@gmail.com>
2025-02-04 22:48:06 -0600

    Time: 2 ms (86.01%), Space: 45.5 MB (59.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,47 +1,46 @@
 class Solution {
+    private int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        int[][] vis = new int[m][n];
-        int[] dRow = {-1, 1, 0, 0};
-        int[] dCol = {0, 0, -1, 1};
+        int rows = board.length;
+        int cols = board[0].length;
+        int[][] vis = new int[rows][cols];
 
-        for(int j = 0; j < n; j++){
-            if(vis[0][j] != 1 && board[0][j] == 'O'){
-                dfs(0, j, vis, board, m, n, dRow, dCol);
+        for (int i = 0; i < cols; i++) {
+            if (vis[0][i] != 1 && board[0][i] == 'O') {
+                dfs(board, vis, 0, i, rows, cols);
             }
-
-            if(vis[m -1][j] != 1 && board[m - 1][j] == 'O'){
-                dfs(m-1, j, vis, board, m, n, dRow, dCol);
+            if (vis[rows - 1][i] != 1 && board[rows - 1][i] == 'O') {
+                dfs(board, vis, rows - 1, i, rows, cols);
             }
         }
 
-        for(int i = 0; i < m; i++){
-            if(vis[i][0] != 1 && board[i][0] == 'O'){
-                dfs(i, 0, vis, board, m, n, dRow, dCol);
+        for (int i = 0; i < rows; i++) {
+            if (vis[i][0] != 1 && board[i][0] == 'O') {
+                dfs(board, vis, i, 0, rows, cols);
             }
-
-            if(vis[i][n-1] != 1 && board[i][n -1] == 'O'){
-                dfs(i, n -1, vis, board, m, n, dRow, dCol);
+            if (vis[i][cols - 1] != 1 && board[i][cols - 1] == 'O') {
+                dfs(board, vis, i, cols - 1, rows, cols);
             }
         }
 
-        for(int i = 0; i < m; i++){
-            for(int j =0; j < n; j++){
-                if(vis[i][j] != 1 && board[i][j] == 'O'){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (vis[i][j] == 0 && board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
         }
     }
 
-    public void dfs(int r, int c, int[][] vis, char[][] board, int m, int n, int[] dRow, int[] dCol){
+    private void dfs(char[][] board, int[][] vis, int r, int c, int rows, int cols) {
         vis[r][c] = 1;
-        for (int i = 0; i < 4; i++) {
-           int nRow = r + dRow[i];
-           int nCol = c + dCol[i];
-            if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && vis[nRow][nCol]!=1 && board[nRow][nCol] == 'O' ) {
-                dfs(nRow, nCol, vis, board, m, n, dRow, dCol);
+        for (int[] d : directions) {
+            int nrow = r + d[0];
+            int ncol = c + d[1];
+            if (nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && vis[nrow][ncol] != 1
+                    && board[nrow][ncol] == 'O') {
+                dfs(board, vis, nrow, ncol, rows, cols);
             }
         }
     }
