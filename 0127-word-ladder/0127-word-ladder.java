18ba3e4f944141d7c9d0af4ec45a010c7a6080d6
jalalm07 <jalalmujawar25@gmail.com>
2025-02-07 09:49:47 -0600

    Time: 91 ms (51.52%), Space: 47 MB (46.46%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,38 +1,37 @@
 class Solution {
-
-    class Pair {
-        String word;
-        int step;
-        Pair(String word, int step){
-            this.word = word;
-            this.step = step;
-        }
+    class Pair{
+            String word;
+            int step;
+            Pair(String word, int step){
+                this.word = word;
+                this.step = step;
+            }
     }
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(beginWord, 1));
         Set<String> set = new HashSet<>();
-        for(int i =0; i < wordList.size(); i++){
-            set.add(wordList.get(i));
+        for(String w: wordList){
+            set.add(w);
         }
         set.remove(beginWord);
 
+        queue.add(new Pair(beginWord, 1));
+
         while(!queue.isEmpty()){
             String word = queue.peek().word;
-            int step = queue.peek().step;
+            int steps = queue.peek().step;
+            if(word.equals(endWord)) return steps;
             queue.remove();
 
-            if(word.equals(endWord)) return step;
-
-            for(int i =0; i < word.length(); i++){
+            for(int i = 0; i < word.length(); i++){
                 for(char ch = 'a'; ch <= 'z'; ch++){
-                    char[] replacedChars = word.toCharArray();
-                    replacedChars[i] = ch;
-                    String replacedString = new String(replacedChars);
-                    if(set.contains(replacedString)){
-                        set.remove(replacedString);
-                        queue.add(new Pair(replacedString, step + 1));
+                    char[] wordChar = word.toCharArray();
+                    wordChar[i] = ch;
+                    String newWord = new String(wordChar);
+                    if(set.contains(newWord)){
+                        queue.add(new Pair(newWord, steps + 1));
+                        set.remove(newWord);
                     }
                 }
             }
