ed7ccf14c36636e9192f269f8a18159461c46320
jalalm07 <jalalmujawar25@gmail.com>
2025-02-16 16:23:02 -0600

    Time: 0 ms (100%), Space: 41.5 MB (18.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[1];
+        if(n == 1) return nums[0];
         return Math.max(f(Arrays.copyOfRange(nums, 0, n - 1)), f(Arrays.copyOfRange(nums, 1, n)));
     }
 
