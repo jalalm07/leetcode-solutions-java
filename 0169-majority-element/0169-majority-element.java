4ce7283b4f7a0c8376a756a311b7b834ddc831c0
jalalm07 <jalalmujawar25@gmail.com>
2025-02-23 17:21:18 -0600

    Time: 22 ms (8.26%), Space: 49.2 MB (92.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,31 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        // using moore's voting algo
-        int major=nums[0], count = 1;
-        for(int i=1; i<nums.length;i++){
-            if(count==0){
-                count++;
-                major=nums[i];
-            }else if(major==nums[i]){
-                count++;
-            }else count--;
-            
+        if(nums.length == 1) return nums[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        int majoritySize = nums.length / 2;
+        for(int n : nums){
+            if(map.containsKey(n)){
+                if(map.get(n) >= majoritySize) return n;
+            }
+            map.put(n, map.getOrDefault(n , 0) + 1);
         }
-        return major;
-
-        // if(nums.length == 1) return nums[0];
-
-        // Map<Integer, Integer> countMap = new HashMap<>();
-        // int mark = nums.length / 2;
-        // for(int num : nums){
-        //     if(countMap.containsKey(num)){
-        //         if(countMap.get(num) + 1 > mark) return num;
-        //         else countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-        //     } else countMap.put(num, 1);
-        // }
-        // return -1;
-
-
+        return -1;
     }
 }
