ebc55679092c180884f215a18bec0b01d7c92a3e
jalalm07 <jalalmujawar25@gmail.com>
2025-03-07 08:03:32 -0600

    Time: 6 ms (66.3%), Space: 46.4 MB (52.77%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -1,16 +1,16 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
@@ -19,25 +19,19 @@ class Solution {
     public int deepestLeavesSum(TreeNode root) {
         Map<Integer, List<Integer>> leafNode = new HashMap<>();
         dfs(root, 0, leafNode);
-        //System.out.println(leafNode);
         int maxDepth = Collections.max(leafNode.keySet());
-
-        // Compute the sum for the deepest depth
         return leafNode.get(maxDepth)
                 .stream()
                 .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    private void dfs(TreeNode root, int level, Map<Integer, List<Integer>> leafNode){
-        if(root == null) return;
-        if(root.left == null && root.right == null){
-            //if(level >= maxL){
-                leafNode.computeIfAbsent(level, p -> new ArrayList<>()).add(root.val);
-                maxL = level;
-                sum += root.val;
-                return;
-            //}
+    private void dfs(TreeNode root, int level, Map<Integer, List<Integer>> leafNode) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            leafNode.computeIfAbsent(level, p -> new ArrayList<>()).add(root.val);
+            return;
         }
         dfs(root.left, level + 1, leafNode);
         dfs(root.right, level + 1, leafNode);
