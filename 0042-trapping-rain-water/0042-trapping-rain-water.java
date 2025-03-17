dadd48d3db5b9c2d45231c67524fa76ee4ba0458
jalalm07 <jalalmujawar25@gmail.com>
2025-03-17 12:40:35 -0500

    Time: 0 ms (100%), Space: 46.6 MB (28.26%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -5,23 +5,15 @@ class Solution {
         int maxL = height[l];
         int maxR = height[r];
         int ans = 0;
-        while(l < r){
-            if(maxL <= maxR){
-                  l++;
-                //if(maxL - height[l] > 0){
-                    maxL = Math.max(maxL, height[l]);
-                    ans += maxL - height[l];
-                //}
-                
-              
+        while (l < r) {
+            if (maxL <= maxR) {
+                l++;
+                maxL = Math.max(maxL, height[l]);
+                ans += maxL - height[l];
             } else {
                 r--;
-                //if(maxR - height[r] > 0){
-                      maxR = Math.max(maxR, height[r]);
-                    ans += maxR - height[r];
-                //}
-              
-            
+                maxR = Math.max(maxR, height[r]);
+                ans += maxR - height[r];
             }
         }
         return ans;
